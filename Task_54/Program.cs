22eb668bb9e8cdea80288876cef167d729e74243
Galina <gal-dej@yandex.ru>
2022-11-13 15:59:23 +0300

    Task has done

--- Task_54/Program.cs
+++ Task_54/Program.cs
@@ -9,3 +9,45 @@
 9 5 3 2
 8 4 4 2
 */
+int[,]CreateArray(int num1, int num2)
+{
+    int[,]array=new int[num1,num2];
+    Random rnd =new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+      for (int j = 0; j < array.GetLength(1); j++)
+      {
+        array[i,j]=rnd.Next(1,9);
+      }  
+    }
+    return array;
+}
+void PrintArray(int[,]array)
+{
+     for (int i = 0; i < array.GetLength(0); i++)
+    {
+      for (int j = 0; j < array.GetLength(1); j++)
+      {
+        Console.Write($"{array[i,j]} ");
+      } 
+      Console.WriteLine(); 
+    }
+    Console.WriteLine();
+}
+
+int [,] ReversArray (int[,]array)
+{
+    int[,]array1 = new int[array.GetLength(0),array.GetLength(1)];
+      for (int i = 0; i < array1.GetLength(0); i++)
+      {
+        for (int j = 0; j < array1.GetLength(1); j++)
+         {
+           array1[i,j] = array[i,array.GetLength(1)-1-j];
+         }  
+      }
+return array1;
+}
+int[,] matrix = CreateArray(4,6);
+PrintArray(matrix);
+int[,] matrixReversed = ReversArray(matrix);
+PrintArray(matrixReversed);
